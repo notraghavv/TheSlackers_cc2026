a133fb7c6a1782134afdd1e87fae20e5cb0f469b
Notragg <pathakraghav255@gmail.com>
2026-05-12 22:08:06 +0530

    Added Map image in .js File adn Centred it

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let mapImg;
+
+function preload() {
+  mapImg = loadImage('assets/MP_Map.png');
+}
+
 function setup() {
-    createCanvas(800, 600);
+  createCanvas(800, 520);
+  imageMode(CENTER);
 }
 
 function draw() {
-    background(220);
+  background(20);
+
+  let imgW = width * 0.9;
+  let imgH = (mapImg.height / mapImg.width) * imgW;
+
+  image(mapImg, width / 2, height / 2, imgW, imgH);
 }
